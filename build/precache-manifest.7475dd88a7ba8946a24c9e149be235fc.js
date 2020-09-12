7482e834c570369660646aa0d4f789ee3c2d38f9
Eduardo Lerma <lalo_pichu22@hotmail.com>
2020-09-12 00:24:57 -0500

    uibuild

---
+++ build/precache-manifest.7475dd88a7ba8946a24c9e149be235fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f1a30a0ef84105d71c6ce8369464c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc713fca59966f4a86f",
+    "url": "/static/css/main.251313f5.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc713fca59966f4a86f",
+    "url": "/static/js/main.1ad3ba10.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
